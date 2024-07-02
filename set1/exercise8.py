7ec108bf3335214677a618d498d9ff3b92b97760
tarikbenaouda <ta.benaouda@esi-sba.dz>
2024-07-02 12:15:09 +0100

    Add exercise8.py

---
+++ set1/exercise8.py
@@ -0,0 +1,10 @@
+from binascii import unhexlify
+
+def detect_aes_ecb(ciphertext):
+    blocks = [ciphertext[i:i+16] for i in range(0, len(ciphertext), 16)]
+    return len(blocks) != len(set(blocks))
+with open('./8.txt') as f:
+    lines = f.readlines()
+    for line in lines: 
+        if detect_aes_ecb(unhexlify(line.strip())):
+            print(line.strip())
