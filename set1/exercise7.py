8a07907e2070c0c7e021067b223fcd5e15f564f5
tarikbenaouda <ta.benaouda@esi-sba.dz>
2024-07-02 12:08:01 +0100

    Add exercise7.py

---
+++ set1/exercise7.py
@@ -0,0 +1,11 @@
+from Crypto.Cipher import AES
+from base64 import b64decode
+
+def decrypt_aes_ecb(ciphertext, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    return cipher.decrypt(ciphertext)
+
+with open('./7.txt', 'r') as f:
+    ciphertext = b64decode(f.read())
+    key = b'YELLOW SUBMARINE'
+    print(decrypt_aes_ecb(ciphertext, key).decode('utf-8'))
