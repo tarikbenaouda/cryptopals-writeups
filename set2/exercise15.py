199de76f6708e0fd08d3b1b223792447c089cbce
tarikbenaouda <ta.benaouda@esi-sba.dz>
2024-07-03 21:09:08 +0100

    Add exercise15.py

---
+++ set2/exercise15.py
@@ -0,0 +1,15 @@
+# PKCS#7 padding validation
+
+
+def validate(plaintext,block_size):
+    if len(plaintext) % block_size != 0:
+        raise Exception("Invalid padding")
+    padding = plaintext[-1]
+    if padding == 0 or padding > block_size:
+        raise Exception("Invalid padding")
+    for i in range(padding):
+        if plaintext[-1-i] != padding:
+            raise Exception("Invalid padding")
+    return True
+
+assert validate(b'helloworldhelloworld\x02\x02',11)
